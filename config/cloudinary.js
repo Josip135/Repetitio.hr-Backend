2f22ad4a9ea8742e0ca133316a2b7c2496a6679f
Josip Počuča <129209254+Josip135@users.noreply.github.com>
2025-04-10 17:18:44 +0200

    Update cloudinary.js

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,8 +1,3 @@
-//daqmfb6df
-//172541939416593
-//test_user1  rqPqnCn7upaoMfsm   mongodb+srv://test_user1:rqPqnCn7upaoMfsm@cluster0.k6hll.mongodb.net/?
-//mongodb+srv://test_user1:rqPqnCn7upaoMfsm@cluster0.k6hll.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-
 import { v2 as cloudinary } from 'cloudinary'
 
 const connectToCloudinary = async () => {
